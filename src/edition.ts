0449e4034a8525430642b17839de58283a7bb1eb
Iain <iain@zora.co>
2022-05-20 15:48:31 -0400

    fix description field

--- src/edition.ts
+++ src/edition.ts
@@ -1,13 +1,14 @@
 import {
   EditionInitialized,
   MediaURIsUpdated,
-} from "../generated/EditionMetadataRenderer/EditionMetadataRenderer";
+  DescriptionUpdated,
+} from "../generated/templates/EditionMetadataRenderer/EditionMetadataRenderer";
 import { EditionMetadata } from "../generated/schema";
 
 export function handleCreatedEdition(event: EditionInitialized): void {
   const targetContract = event.params.target;
   const metadata = new EditionMetadata(targetContract.toHex());
-  
+
   metadata.description = event.params.description;
   metadata.animationURI = event.params.animationURI;
   metadata.drop = event.params.target.toHex();
@@ -20,6 +21,14 @@ export function handleUpdateMediaURIs(event: MediaURIsUpdated): void {
   if (metadata) {
     metadata.animationURI = event.params.animationURI;
     metadata.imageURI = event.params.imageURI;
+    metadata.save();
+  }
+}
+
+export function handleUpdateDescription(event: DescriptionUpdated): void {
+  const metadata = EditionMetadata.load(event.params.target.toHex());
+  if (metadata) {
+    metadata.description = event.params.newDescription;
     metadata.save();
   }
 }
