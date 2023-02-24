08376de88b1f4353b04d3e920478c40ab5da7ae4
Isabella Smallcombe <isabella@ourzora.com>
2023-02-24 15:02:13 -0500

    feat: update config and mappings

--- src/ERC1155Mappings/factoryMappings.ts
+++ src/ERC1155Mappings/factoryMappings.ts
@@ -1,10 +1,9 @@
 import {
   SetupNewContract,
   Upgraded,
-  ZoraCreator1155FactoryImpl,
 } from "../../generated/ZoraNFTCreatorFactory1155/ZoraCreator1155FactoryImpl";
 
-import { ZoraCreateContract } from "../../generated/schema";
+import { Upgrade, ZoraCreateContract } from "../../generated/schema";
 
 export function handleNewContractCreated(event: SetupNewContract): void {
   const contractId = event.params.newContract.toHex();
@@ -13,13 +12,24 @@ export function handleNewContractCreated(event: SetupNewContract): void {
   createdContract.contractStandard = "ERC1155";
   createdContract.contractURI = event.params.contractURI;
 
-  //    id: ID!;
-  //    name: String!;
-  //    symbol: String;
-  //    contractURI: String!;
-  //    profileImage: String!;
-  //    contractStandard: String!;
-  //    metadata: MetadataInfo;
+  createdContract.name = null;
+  createdContract.symbol = null;
+  createdContract.metadata = null;
+
+  createdContract.save();
 }
 
-export function handle1155FactoryUpgraded(event: Upgraded): void {}
+export function handle1155FactoryUpgraded(event: Upgraded): void {
+  //  const upgrade = new Upgrade(evt.transaction.hash.toHex());
+
+  //  const creator = ZoraNFTCreatorV1.bind(evt.address);
+
+  //  DropMetadataRendererFactory.create(creator.dropMetadataRenderer());
+  //  EditionMetadataRendererFactory.create(creator.editionMetadataRenderer());
+
+  //  upgrade.save();
+  // const upgrade = new Upgrade(event.transaction.hash.toHex());
+  // //const contract = new event.address();
+
+  // upgrade.save();
+}
--- src/ERC1155Mappings/templates/ZoraMerkleMinterStrategyMappings.ts
+++ src/ERC1155Mappings/templates/ZoraMerkleMinterStrategyMappings.ts
@@ -1,3 +1,15 @@
+import { ERC1155SalesConfigMerkleMinterStrategy } from "../../../generated/schema";
 import { SaleSet } from "../../../generated/templates/ZoraCreatorMerkleMinterStrategy/ZoraCreatorMerkleMinterStrategy";
 
-export function handleMerkleMinterStrategySaleSet(event: SaleSet): void {}
+export function handleMerkleMinterStrategySaleSet(event: SaleSet): void {
+ 
+  const id = `-${event.address}-${event.params.tokenId}`; // fix
+  const sale = new ERC1155SalesConfigMerkleMinterStrategy(id);
+  sale.presaleStart = event.params.merkleSaleSettings.presaleStart;
+  sale.presaleEnd = event.params.merkleSaleSettings.presaleEnd;
+  sale.fundsRecipient = event.params.merkleSaleSettings.fundsRecipient;
+  sale.merkleRoot = event.params.merkleSaleSettings.merkleRoot;
+  sale.updatedAt = Date.now();
+
+  sale.save();
+}
