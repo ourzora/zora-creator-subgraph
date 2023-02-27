f6eecf57fa3fc1a2de69c4898a94b167737936e0
Isabella Smallcombe <isabella@ourzora.com>
2023-02-27 11:55:05 -0500

    feat: adding handler for upgrade events for 1155

--- src/ERC1155Mappings/factoryMappings.ts
+++ src/ERC1155Mappings/factoryMappings.ts
@@ -1,10 +1,20 @@
 import {
   SetupNewContract,
   Upgraded,
+  ZoraCreator1155FactoryImpl,
 } from "../../generated/ZoraNFTCreatorFactory1155/ZoraCreator1155FactoryImpl";
 
-import { ZoraCreateContract } from "../../generated/schema";
-import { ZoraCreator1155Impl as ZoraCreator1155ImplTemplate, MetadataInfo as MetadataInfoTemplate } from "../../generated/templates";
+import {
+  Upgrade,
+  ZoraCreate1155Factory,
+  ZoraCreateContract,
+} from "../../generated/schema";
+import {
+  ZoraCreator1155Impl as ZoraCreator1155ImplTemplate,
+  MetadataInfo as MetadataInfoTemplate,
+  ZoraCreatorFixedPriceSaleStrategy,
+  ZoraCreatorMerkleMinterStrategy,
+} from "../../generated/templates";
 import { makeTransaction } from "../common/makeTransaction";
 import { getIPFSHostFromURI } from "../common/getIPFSHostFromURI";
 import { BigInt } from "@graphprotocol/graph-ts";
@@ -35,21 +45,20 @@ export function handleNewContractCreated(event: SetupNewContract): void {
   ZoraCreator1155ImplTemplate.create(event.params.newContract);
 }
 
-export function handle1155FactoryUpgraded(event: Upgraded): void {}
-//  const upgrade = new Upgrade(evt.transaction.hash.toHex());
+export function handle1155FactoryUpgraded(event: Upgraded): void {
+  const upgrade = new Upgrade(event.transaction.hash.toHex());
+  const factory = new ZoraCreate1155Factory(event.address.toHex());
+  const creator = ZoraCreator1155FactoryImpl.bind(event.address);
+  
+  ZoraCreatorFixedPriceSaleStrategy.create(creator.defaultMinters()[0]);
+  ZoraCreatorMerkleMinterStrategy.create(creator.defaultMinters()[1]);
 
-//  const creator = ZoraNFTCreatorV1.bind(evt.address);
+  factory.txn = makeTransaction(event);
+  factory.fixedPriceSaleStrategyAddress = creator.defaultMinters()[0];
+  factory.implementation = event.params.implementation;
+  factory.merkleSaleStrategyAddress = creator.defaultMinters()[1];
+  factory.version = creator.contractVersion();
 
-//  DropMetadataRendererFactory.create(creator.dropMetadataRenderer());
-//  EditionMetadataRendererFactory.create(creator.editionMetadataRenderer());
-
-//  upgrade.save();
-// const upgrade = new Upgrade(event.transaction.hash.toHex());
-
-// //const contract = new event.address();
-
-// do the create for the sales confisgs
-// .create means listen for that new address
-
-// upgrade.save();
-// }
+  upgrade.save();
+  factory.save();
+}
