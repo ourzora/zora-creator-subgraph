f7dde273b0101d26069d5c6cd86decbdf573a3cf
kulkarohan <kulkarohan@gmail.com>
2023-05-31 22:39:04 -0400

    chore: remove semicolons

--- src/ERC1155Mappings/templates/ZoraMerkleMinterStrategyMappings.ts
+++ src/ERC1155Mappings/templates/ZoraMerkleMinterStrategyMappings.ts
@@ -20,7 +20,7 @@ export function handleMerkleMinterStrategySaleSet(event: SaleSet): void {
   const txn = makeTransaction(event);
   sale.txn = txn;
   sale.tokenId = event.params.tokenId
-  sale.contract = event.params.mediaContract.toHex();;
+  sale.contract = event.params.mediaContract.toHex()
 
   sale.save();
 
