571c09757c5243ea391f5e7000d565581f11c7a9
Rohan Kulkarni <kulkarohan@gmail.com>
2023-08-01 14:00:47 -0400

    feat(wip): rewards handlers

---
+++ src/ProtocolRewardsMappings/protocolRewardsMappings.ts
@@ -0,0 +1,9 @@
+import { RewardsDeposit, Deposit, Withdraw } from '../../generated/schema'
+
+import { makeTransaction } from "../common/makeTransaction";
+
+export function handleRewardsDeposit(event: RewardsDeposit): void {}
+
+export function handleDeposit(event: Deposit): void {}
+
+export function handleWithdraw(event: Withdraw): void {}
