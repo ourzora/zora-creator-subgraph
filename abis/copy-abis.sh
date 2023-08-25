5bb2359fbb8b80bedb9755a92f1a3c963d5c3931
Rohan Kulkarni <kulkarohan@gmail.com>
2023-08-24 23:08:47 -0400

    refactor: remove protocol rewards abi from copy script

--- abis/copy-abis.sh
+++ abis/copy-abis.sh
@@ -2,7 +2,6 @@ REL_BASE=$(dirname "$0")
 
 ERC721_ARTIFACTS_PATH=$REL_BASE/../node_modules/@zoralabs/nft-drop-contracts/dist/artifacts/
 ERC1155_ARTIFACTS_PATH=$REL_BASE/../node_modules/@zoralabs/zora-1155-contracts/dist/artifacts/
-PROTOCOL_REWARDS_ARTIFACTS_PATH=$REL_BASE/../node_modules/@zoralabs/protocol-rewards/dist/artifacts/
 
 get_contract () {
   node -e 'var fs = require("fs");console.log(JSON.stringify(JSON.parse(fs.readFileSync(process.argv[1])).abi, null, 2))' $1/$2.sol/$2.json > $REL_BASE/$2.json
@@ -27,6 +26,3 @@ get_contract $ERC1155_ARTIFACTS_PATH 'ZoraCreatorFixedPriceSaleStrategy'
 get_contract $ERC1155_ARTIFACTS_PATH 'ZoraCreatorMerkleMinterStrategy'
 get_contract $ERC1155_ARTIFACTS_PATH 'ZoraCreatorRedeemMinterFactory'
 get_contract $ERC1155_ARTIFACTS_PATH 'ZoraCreatorRedeemMinterStrategy'
-
-# protocol rewards contract
-get_contract $PROTOCOL_REWARDS_ARTIFACTS_PATH 'ProtocolRewards'
