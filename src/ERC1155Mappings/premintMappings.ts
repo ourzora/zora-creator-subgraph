14205b4e3f6524924973e676b8ad0f616133ff0b
Isabella Smallcombe <isabella@ourzora.com>
2023-12-11 19:27:17 -0500

    feat: add premint v2 event

--- src/ERC1155Mappings/premintMappings.ts
+++ src/ERC1155Mappings/premintMappings.ts
@@ -1,5 +1,5 @@
 import { Premint } from "../../generated/schema";
-import { Preminted } from "../../generated/ZoraCreator1155PremintExecutorV1/ZoraCreator1155PremintExecutorImpl";
+import { Preminted, PremintedV2 } from "../../generated/ZoraCreator1155PremintExecutorV1/ZoraCreator1155PremintExecutorImpl";
 import { getTokenId } from "../common/getTokenId";
 
 export function handlePreminted(event: Preminted): void {
@@ -15,6 +15,24 @@ export function handlePreminted(event: Preminted): void {
     event.params.tokenId
   );
   premint.createdNewContract = event.params.createdNewContract;
+
+  premint.save();
+}
+
+export function handlePremintedV2(event: PremintedV2): void {
+  const premint = new Premint(
+    `${event.params.contractAddress.toHex()}-${event.params.tokenId.toHex()}-${event.params.minter.toHex()}}`
+  );
+  premint.uid = event.params.uid;
+  premint.contractAddress = event.params.contractAddress;
+  premint.tokenId = event.params.tokenId;
+  premint.minter = event.params.minter;
+  premint.tokenAndContract = getTokenId(
+    event.params.contractAddress,
+    event.params.tokenId
+  );
+  premint.createdNewContract = event.params.createdNewContract;
+  premint.quantityMinted = event.params.quantityMinted;
 
   premint.save();
 }
