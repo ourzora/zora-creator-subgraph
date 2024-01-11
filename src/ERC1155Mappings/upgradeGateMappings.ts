d91dee19c457c06f4b16d561141fa9e05c69e1a1
Dan Oved <stangogh@gmail.com>
2024-01-10 18:33:48 -0800

    Added subgraph for upgrade gate

---
+++ src/ERC1155Mappings/upgradeGateMappings.ts
@@ -0,0 +1,45 @@
+import {
+  UpgradeRegistered, UpgradeRemoved,
+} from "../../generated/UpgradePathsV1/UpgradeGate";
+
+import {
+  ContractVersion,
+  UpgradePath,
+} from "../../generated/schema";
+import { ZoraCreator1155Impl } from "../../generated/templates/ZoraCreator1155Impl/ZoraCreator1155Impl";
+import { Bytes, store } from "@graphprotocol/graph-ts";
+
+function getContractVersion(address: Bytes): ContractVersion {
+  const contractVersion = new ContractVersion(address);
+
+  const versionString = ZoraCreator1155Impl.bind(address).contractVersion();
+
+  contractVersion.version = versionString;
+
+  return contractVersion;
+}
+
+function upgradePathId(from: Bytes, to: Bytes): string {
+  return `${from.toHex()}-${to.toHex()}`;
+}
+
+export function handleUpgradeRegistered(event: UpgradeRegistered): void {
+  const fromVersion = getContractVersion(event.params.baseImpl);
+  const toVersion = getContractVersion(event.params.upgradeImpl);
+  
+  const upgradePath = new UpgradePath(upgradePathId(event.params.baseImpl, event.params.upgradeImpl));
+  upgradePath.from = fromVersion.id;
+  upgradePath.to = toVersion.id;
+
+  fromVersion.save();
+  toVersion.save();
+  upgradePath.save();
+}
+
+export function handleUpgradeRemoved(event: UpgradeRemoved): void {
+  const upgradePath = UpgradePath.load(upgradePathId(event.params.baseImpl, event.params.upgradeImpl));
+
+  if (upgradePath)
+    store.remove("UpgradePath", upgradePath.id);
+}
+
